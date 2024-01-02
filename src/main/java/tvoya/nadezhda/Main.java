be088d3121fa902714bb3ffc482b84750bad852f
Tvoya.Nadezhda <tvoya.nadezhd@yandex.ru>
2024-01-02 20:34:02 +0300

    Your short summary about the commit

--- src/main/java/tvoya/nadezhda/Main.java
+++ src/main/java/tvoya/nadezhda/Main.java
@@ -1,12 +1,14 @@
 package tvoya.nadezhda;
 
 
+
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
         private static boolean getMessage (){
-        return "hello and!!!"
+        return "11111" ;
     }
 }
